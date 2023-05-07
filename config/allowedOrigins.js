dd65dbf97854bd488409a2bd3f6d9eb9959ee834
Toan Dinh <youquand21@gmail.com>
2023-05-07 11:44:41 -0400

    removed vercel settings

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
 	'http://localhost:3000',
-	'https://instagram-clone-frontend-toandnh.vercel.app',
-	'https://instagram-clone-frontend-theta.vercel.app',
-	'https://instagram-clone-frontend-5cq8tvch6-toandnh.vercel.app'
+	'https://instagram-clone-frontend-toandnh.vercel.app'
 ]
 
 module.exports = allowedOrigins
