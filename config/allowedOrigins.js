b0ce9f09bbde9c375e9fbbcd308807538c7ea27b
Toan Dinh <youquand21@gmail.com>
2023-05-07 06:31:36 -0400

    started tracking uploads folder

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const allowedOrigins = [
+	'http://localhost:3000',
 	'http://instagram-clone-frontend-toandnh.vercel.app/',
 	'https://instagram-clone-frontend-theta.vercel.app/',
 	'https://instagram-clone-frontend-5cq8tvch6-toandnh.vercel.app'
