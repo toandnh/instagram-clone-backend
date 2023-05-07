fe7f06114755f4179ed2166194117b85769cba81
Toan Dinh <youquand21@gmail.com>
2023-05-07 06:54:36 -0400

    another typo

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
 	'http://localhost:3000',
-	'https://instagram-clone-frontend-toandnh.vercel.app/',
-	'https://instagram-clone-frontend-theta.vercel.app/',
+	'https://instagram-clone-frontend-toandnh.vercel.app',
+	'https://instagram-clone-frontend-theta.vercel.app',
 	'https://instagram-clone-frontend-5cq8tvch6-toandnh.vercel.app'
 ]
 
