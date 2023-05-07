ffee7d41f8af528c518a28e3b257b928809d559e
Toan Dinh <youquand21@gmail.com>
2023-05-07 05:54:54 -0400

    added allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:3000'
+	'http://instagram-clone-frontend-toandnh.vercel.app/',
+	'https://instagram-clone-frontend-theta.vercel.app/',
+	'https://instagram-clone-frontend-5cq8tvch6-toandnh.vercel.app'
 ]
 
 module.exports = allowedOrigins
